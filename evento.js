5513bf3cf5e56185b88a3a544896c43081ff0f61
jochefini <mjgcoppola@gmail.com>
2024-08-31 14:31:09 -0300

    solucion click

--- evento.js
+++ evento.js
@@ -1,7 +1,15 @@
+
 function mostrarAlerta() {
     alert('Hola! soy el div');
 }
+
 document.addEventListener('DOMContentLoaded', function() {
-const contenedor = document.querySelector('.contenedor');
+    const contenedor = document.querySelector('.contenedor');
     contenedor.addEventListener('click', mostrarAlerta);
+
+    const boton = document.querySelector('.boton');
+    boton.addEventListener('click', function(event) {
+        event.stopPropagation();
+        alert('Hola!');
+    });
 });
