6ca869bf66aab51d4fc496d2584098af58058619
jochefini <mjgcoppola@gmail.com>
2024-08-31 14:12:01 -0300

    .

--- evento.js
+++ evento.js
@@ -1,5 +1,7 @@
 function mostrarAlerta() {
     alert('Hola! soy el div');
 }
+document.addEventListener('DOMContentLoaded', function() {
 const contenedor = document.querySelector('.contenedor');
     contenedor.addEventListener('click', mostrarAlerta);
+});
